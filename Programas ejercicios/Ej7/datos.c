d71a421e3c86af00924ed74f64ca0a60ccab7cee
Mosorior <mosorior173@gmail.com>
2022-12-13 10:17:43 +0100

    Deployed 444202a with MkDocs version: 1.4.2

---
+++ Programas ejercicios/Ej7/datos.c
@@ -0,0 +1,8 @@
+//-------------
+// datos.c
+//-------------
+
+char *mensaje="Hola a todos y todas";
+int  num1 = 8;
+int  num2 = 10;
+
---
+++ Programas ejercicios/Ej7/suma.c
@@ -0,0 +1,8 @@
+ 
+//-------------
+// suma.c
+//-------------
+
+int suma (int a, int b) {
+  return a + b;
+}
