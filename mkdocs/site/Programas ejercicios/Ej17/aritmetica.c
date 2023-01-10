3404f75d2ec3bbfabb9e2a66f2adf47621c923b8
Mosorior <mosorior173@gmail.com>
2023-01-10 09:47:21 +0100

    mkdocs.yml actualizado con el tema 7

---
+++ mkdocs/site/Programas ejercicios/Ej17/aritmetica.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int suma (int a, int b){
+    return a+b;
+}
+int resta (int a, int b){
+   return a-b;
+}
+int multiplicar (int a, int b){
+   return a*b;
+}
+int dividir (int a, int b){
+   return a/b;
+}
